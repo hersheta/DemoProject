99319dc4b53a96049d0a8f81989e39a222593ef7
Harshita Srivastava <harshitasrivastava@Harshitas-MacBook-Air.local>
2024-05-27 11:23:21 +0530

    commit all changes

--- JavaProgram/src/main/java/org/example/h.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class h {
-    public static void main(String[] args) {
-        System.out.println("hello java");
-    }
-}
--- JavaProgram/src/main/java/org/example/oop.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class oop {
-    public static void main(String[] args) {
-        System.out.println("dsfhjsf");
-    }
-}
